f529b382e73928fe23d3f6d9778ab100e53e152e
felipeapp@gmail.com <felipeapp@gmail.com@052268ec-9ec3-845a-e114-2c86d2a41a4c>
2014-04-23 16:43:17 +0000

    Script BAT atualizado.

--- trunk/br.ufrn.ppgsc.scenario.analyzer.tests/scripts/run_ant_file.bat
+++ trunk/br.ufrn.ppgsc.scenario.analyzer.tests/scripts/run_ant_file.bat
@@ -1,11 +1,11 @@
-:: This file will run an ant build file N times
-:: How to run:
-::		run_ant_file.bat target n [file]
-:: target: an ant target from build file
-:: n: number of executions
-:: file: an file to log stdout and stderr
-
 @echo off
+
+REM This file will run an ant build file N times
+REM How to run:
+REM		run_ant_file.bat target n [file]
+REM target: an ant target from build file
+REM n: number of executions
+REM file: an file to log stdout and stderr
 
 IF "%1" == "" GOTO HelpMessage
 IF "%2" == "" GOTO HelpMessage
@@ -14,15 +14,15 @@ FOR /L %%I IN (1,1,%2) DO (
 	echo "%%I: Running ant build file..."
 	IF "%3" == "" (
 		echo "%%I: ant %1"
-		ant %1
+		cmd /c ant %1
 	) ELSE (
 		echo "%%I: ant %1 > exec%%I_%3 2>&1"
-		ant %1 > exec%%I_%3 2>&1
+		cmd /c ant %1 > exec%%I_%3 2>&1
 	)
 	
 	echo "%%I: Dumping database..."
 	echo "%%I: pg_dump --host localhost --port 5432 --username postgres --no-password --format custom --blobs --file scenario_analyzer_db_%%I.backup scenario_analyzer_db"
-	pg_dump --host localhost --port 5432 --username postgres --no-password --format custom --blobs --file scenario_analyzer_db_%%I.backup scenario_analyzer_db
+	cmd /c pg_dump --host localhost --port 5432 --username postgres --no-password --format custom --blobs --file scenario_analyzer_db_%%I.backup scenario_analyzer_db
 )
 
 GOTO EndScript
@@ -32,3 +32,4 @@ echo "Ex 1: run_ant_file.bat tests 10"
 echo "Ex 2: run_ant_file.bat tests 10 tests.txt"
 
 :EndScript
+echo "Bye..."
