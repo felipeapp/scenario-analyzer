f70d399f0ce1a33323679cb08e78148d40c71dcf
felipeapp@gmail.com <felipeapp@gmail.com@052268ec-9ec3-845a-e114-2c86d2a41a4c>
2014-05-06 13:54:12 +0000

    Resultados do Netty.

---
+++ trunk/br.ufrn.ppgsc.scenario.analyzer.tests/src/icsme/MainICSME.java
@@ -0,0 +1,36 @@
+package icsme;
+
+import br.ufrn.ppgsc.scenario.analyzer.common.annotations.Performance;
+import br.ufrn.ppgsc.scenario.analyzer.common.annotations.arq.Scenario;
+
+
+public class MainICSME {
+
+	public static void main(String[] args) {
+		User u = new User(true, false, true);
+		MainICSME m = new MainICSME(u);
+		System.out.println(m.checkUser(10));
+	}
+	
+	public MainICSME(User u) {
+		this.u = u;
+	}
+	
+	private User u;
+	
+	@Scenario(name = "Check User Status")
+	public boolean checkUser(long id) {
+		u = retrieveUserInformation(id);
+		return !hasIrregularity() && u.isActive();
+	}
+
+	public boolean hasIrregularity()
+	{ return u.isOverdue() && u.hasFine(); }
+	
+	@Performance(
+		name = "Alerto for retrieveUserInformation",
+		limitTime = 100
+	)
+	public User retrieveUserInformation(long id) { return null; }
+
+}
