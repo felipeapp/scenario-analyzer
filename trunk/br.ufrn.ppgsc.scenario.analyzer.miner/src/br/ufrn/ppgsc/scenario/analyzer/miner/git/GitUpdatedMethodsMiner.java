891429f65bec086ce5b8bf8738aa72457d2d1198
felipeapp@gmail.com <felipeapp@gmail.com@052268ec-9ec3-845a-e114-2c86d2a41a4c>
2014-08-11 19:44:42 +0000

    Refatoração do minerador (SVN e Git). SVN testado.

--- trunk/br.ufrn.ppgsc.scenario.analyzer.miner/src/br/ufrn/ppgsc/scenario/analyzer/miner/git/GitUpdatedMethodsMiner.java
+++ trunk/br.ufrn.ppgsc.scenario.analyzer.miner/src/br/ufrn/ppgsc/scenario/analyzer/miner/git/GitUpdatedMethodsMiner.java
@@ -1,91 +1,54 @@
 package br.ufrn.ppgsc.scenario.analyzer.miner.git;
 
-import java.util.Collection;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.log4j.Logger;
+import java.util.Properties;
+import java.util.Set;
 
 import br.ufrn.ppgsc.scenario.analyzer.miner.ifaces.IRepositoryMiner;
 import br.ufrn.ppgsc.scenario.analyzer.miner.model.UpdatedLine;
-import br.ufrn.ppgsc.scenario.analyzer.miner.model.UpdatedMethod;
-import br.ufrn.ppgsc.scenario.analyzer.miner.parser.MethodLimitBuilder;
+import br.ufrn.ppgsc.scenario.analyzer.miner.util.SystemMetadataUtil;
 
 public class GitUpdatedMethodsMiner implements IRepositoryMiner {
-
-	private final Logger logger = Logger.getLogger(GitUpdatedMethodsMiner.class);
 	
-	private Map<String, Collection<UpdatedMethod>> changedMethods;
+	private Properties gitcommits;
+	
 	private Map<String, GitUpdatedLinesHandler> handlers;
 	
 	private String url;
     private String user;
     private String password;
     
-    private List<String> targetPaths;
-	private List<String> startRevisions;
-	private List<String> endRevisions;
-    
-	public void connect(String url, String user, String password) {
-		this.url = url;
+    public void connect(String url, String user, String password) {
+    	this.url = url;
 		this.user = user;
 		this.password = password;
-	}
-	
-	public void initialize(List<String> targetPaths, List<String> startRevisions, List<String> endRevisions) {
-		this.targetPaths = targetPaths;
-		this.startRevisions = startRevisions;
-		this.endRevisions = endRevisions;
-		
+    }
+    
+    public void initialize() {
 		this.handlers = new HashMap<String, GitUpdatedLinesHandler>();
-		this.changedMethods = new HashMap<String, Collection<UpdatedMethod>>();
+		
+		this.gitcommits = new Properties();
+		try {
+			this.gitcommits.load(new FileInputStream(SystemMetadataUtil.getInstance().getStringProperty("commit_file")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public Map<String, Collection<UpdatedMethod>> mine() {
-		logger.info("starting mining...");
+	public Object mine(String path, String startRevision, String endRevision) {
+		GitUpdatedLinesHandler handler  = new GitUpdatedLinesHandler(
+				startRevision,
+				endRevision,
+				url + path.substring(0, path.lastIndexOf('/')),
+				path.substring(path.lastIndexOf('/') + 1));
 		
-		int i = 0;
-		for (String path : targetPaths) {
-			if (!startRevisions.get(i).equals(endRevisions.get(i))) {
-				logger.info("Running doAnnotate [" + startRevisions.get(i) + ", " + endRevisions.get(i) + "]");
-				logger.info("Path (" + (i + 1) + "/" + targetPaths.size() + "):" + path);
-				
-				GitUpdatedLinesHandler handler = handlers.get(path);
-				
-				if (handler == null) {
-					handler = new GitUpdatedLinesHandler(
-							startRevisions.get(i),
-							endRevisions.get(i),
-							url + targetPaths.get(i).substring(0, targetPaths.get(i).lastIndexOf('/')),
-							targetPaths.get(i).substring(targetPaths.get(i).lastIndexOf('/') + 1));
-					
-					handler.calculateChangedLines();
-					
-					handlers.put(path, handler);
-				}
-				else {
-					logger.info("Path previously analyzedPath: " + path);
-				}
-			}
-			
-			++i;
-		}
+		handler.calculateChangedLines();
 		
-		for (String path : handlers.keySet()) {
-			GitUpdatedLinesHandler handler = handlers.get(path);
-			
-			// Pega as linhas modificadas
-			List<UpdatedLine> lines = handler.getChangedLines();
-			
-			// Parse da classe buscando os métodos (linha inicial, final, nome)
-			MethodLimitBuilder builder = new MethodLimitBuilder(handler.getSourceCode());
-			
-			// Pega os métodos mudados verificando as linhas mudadas e os limites dos métodos
-			changedMethods.put(path, builder.filterChangedMethods(lines));
-		}
-		
-		return changedMethods;
+		return handler;
 	}
 	
 	public String getUrl() {
@@ -99,27 +62,36 @@ public class GitUpdatedMethodsMiner implements IRepositoryMiner {
 	public String getPassword() {
 		return password;
 	}
+	
+	public Object getLinesHandler(String path) {
+		return handlers.get(path);
+	}
 
-	//TODO: Deixar esse método genérico
+	public Object putLinesHandler(String path, Object handler) {
+		return handlers.put(path, (GitUpdatedLinesHandler) handler);
+	}
+	
+	public Set<String> getAllLinesHandlerKeys() {
+		return handlers.keySet();
+	}
+
+	public List<UpdatedLine> getChangedLines(String path) {
+		return handlers.get(path).getChangedLines();
+	}
+
+	public String getSourceCode(String path) {
+		return handlers.get(path).getSourceCode();
+	}
+
 	public String getCommittedRevisionNumber(String path) {
-		if (path.contains("netty-4.0.0.Final"))
-			return "c5d8af446a39db10a1744d47e5a466fa1c87a374";
-		else if (path.contains("netty-4.0.6.Final"))
-			return "3f70d5caa4bf0201ff1861e8dcb111a537a4a64c";
-		else if (path.contains("netty-4.0.10.Final"))
-			return "d35768ae117e7f9f20fca5b5297c96019b9a574a";
-		else if (path.contains("netty-4.0.15.Final"))
-			return "287515210d2215d8355a60809871fe09ac943f10";
-		else if (path.contains("netty-4.0.17.Final"))
-			return "33587eb18357dfece421167aea52cd722ee101f9";
-		else if (path.contains("netty-4.0.18.Final"))
-			return "1512a4dccacfd9bf14f06b5c96b2ec46d22760c2";
-		else if (path.contains("netty-4.0.19.Final"))
-			return "b562148e2d8d0f0487495fb5dd2d5de62306c5e0";
-		else if (path.contains("netty-4.0.21.Final"))
-			return "25e3c8ce3d06bcf470ee4493c5744a4c955601cc";
-		else
-			return null;
+		for (Object obj : gitcommits.keySet()) {
+			String key = (String) obj;
+			
+			if (path.contains(key))
+				return gitcommits.getProperty(key);
+		}
+		
+		return null;
 	}
 
 }
--- trunk/br.ufrn.ppgsc.scenario.analyzer.miner/src/br/ufrn/ppgsc/scenario/analyzer/miner/ifaces/IRepositoryMiner.java
+++ trunk/br.ufrn.ppgsc.scenario.analyzer.miner/src/br/ufrn/ppgsc/scenario/analyzer/miner/ifaces/IRepositoryMiner.java
@@ -1,25 +1,34 @@
 package br.ufrn.ppgsc.scenario.analyzer.miner.ifaces;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
-import br.ufrn.ppgsc.scenario.analyzer.miner.model.UpdatedMethod;
+import br.ufrn.ppgsc.scenario.analyzer.miner.model.UpdatedLine;
 
 public interface IRepositoryMiner {
 
 	public void connect(String url, String user, String password);
 
-	public void initialize(List<String> targetPaths, List<String> startRevisions, List<String> endRevisions);
+	public void initialize();
 
-	public Map<String, Collection<UpdatedMethod>> mine();
+	public Object mine(String path, String startRevision, String endRevision);
 
 	public String getCommittedRevisionNumber(String path);
 
-//	public String getUrl();
-//
-//	public String getUser();
-//
-//	public String getPassword();
+	public Object getLinesHandler(String path);
+
+	public Object putLinesHandler(String path, Object handler);
+
+	public Set<String> getAllLinesHandlerKeys();
+
+	public List<UpdatedLine> getChangedLines(String path);
+
+	public String getSourceCode(String path);
+
+	// public String getUrl();
+	//
+	// public String getUser();
+	//
+	// public String getPassword();
 
 }
