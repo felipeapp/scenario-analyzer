3e23e676609d1919a738eb59c955440d109890e1
felipeapp@gmail.com <felipeapp@gmail.com@052268ec-9ec3-845a-e114-2c86d2a41a4c>
2015-01-09 23:22:27 +0000

    Versão funcional usada no primeiro estudo do SIGAA, no ArgoUML e no Netty. Essa versão foi criada antes das modificações para regredir o hibernate para rodar a ferramenta no cenário de matrícula do SIGAA .

---
+++ tags/R_0.2/br.ufrn.ppgsc.scenario.analyzer.tests/src/tests/Main2.java
@@ -0,0 +1,62 @@
+package tests;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import br.ufrn.ppgsc.scenario.analyzer.common.annotations.Performance;
+import br.ufrn.ppgsc.scenario.analyzer.common.annotations.Reliability;
+import br.ufrn.ppgsc.scenario.analyzer.common.annotations.Security;
+import br.ufrn.ppgsc.scenario.analyzer.common.annotations.arq.Scenario;
+
+
+public class Main2 extends TestCase {
+
+	public static void main(String[] args) {
+		Main2 o = new Main2();
+
+		o.testB();
+		o.testA();
+	}
+
+	private Main2() {
+
+	}
+
+//	@Scenario(name = "Scenario A")
+	@Test
+	@Performance(name = "qa_a_performance", limitTime = 10)
+	public void testA() {
+		testB();
+		C();
+		new Main2();
+	}
+
+	@Performance(name = "qa_b_performance", limitTime = 4)
+	@Reliability(name = "qa_b_reliability", failureRate = 0.1)
+	@Test
+	@Scenario(name = "Scenario B")
+	private int testB() {
+		C();
+		return 0;
+	}
+
+	@Scenario(name = "Scenario C")
+	@Test
+	public void C() {
+		D();
+	}
+
+	@Performance(name = "qa_d_performance", limitTime = 5)
+	@Reliability(name = "qa_d_reliability", failureRate = 0.01)
+	@Security(name = "qa_d_security")
+	public void D() {
+		try {
+			System.out.println(1 / 0);
+		} catch (ArithmeticException e) {
+			System.out.println("Exceção em D!");
+		}
+		
+		System.out.println("Aqui em D!");
+	}
+
+}
