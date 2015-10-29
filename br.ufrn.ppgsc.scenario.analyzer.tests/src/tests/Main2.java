f235d9218b8b22cbc350e3b3afbb5dffa668d11a
felipe_app <felipe_app@yahoo.com.br>
2015-10-29 17:27:39 -0300

    Atualização do exemplo.

--- br.ufrn.ppgsc.scenario.analyzer.tests/src/tests/Main2.java
+++ br.ufrn.ppgsc.scenario.analyzer.tests/src/tests/Main2.java
@@ -3,9 +3,11 @@ package tests;
 import junit.framework.TestCase;
 
 import org.junit.Test;
+import org.springframework.stereotype.Component;
 
 import br.ufrn.ppgsc.scenario.analyzer.common.annotations.arq.Scenario;
 
+@Component
 public class Main2 extends TestCase {
 
 	@Test
